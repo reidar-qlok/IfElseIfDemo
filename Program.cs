5150887f997222f8c011e048df64e17b5aada413
Reidar Nilsen <4reidar@gmail.com>
2024-09-09 15:26:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IfElseIfDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 51;
+            if (myNumber > 50)
+            {
+                Console.WriteLine($"Utskrift: {myNumber} är större än 50");
+            }
+            else if (myNumber < 50)
+            {
+                Console.WriteLine("Utskrift: " + myNumber + " är mindre än 50");
+            }
+            else
+            {
+                Console.WriteLine("myNumber är lika med 50");
+            }
+        }
+    }
+}
